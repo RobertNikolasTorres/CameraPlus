c184a578d6c6c7534616c9f5a0faf10354bc14cd
robert <robert.nikolas.torres@gmail.com>
2025-10-04 16:06:48 -0400

    Initial Commit

---
+++ CameraPlus/CameraPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPlusApp.swift
+//  CameraPlus
+//
+//  Created by Robert Nikolas Torres on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
